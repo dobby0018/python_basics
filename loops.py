922862983a5929ddce412d794e0b8c3a3b1ce536
Abhinav Lotlikar <lotlikarabhinav@gmail.com>
2023-10-11 22:42:42 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,11 @@
+fruits=["apple","orange","pear"]
+for fruit in fruits:
+    print(fruit)
+    
+for i in range(1,6):
+    if i==4:
+        continue
+    print(i)
+for i in range(3):
+    
+        print(f"{i}:{fruits[i]}")
